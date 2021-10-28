d1ec849f3a4a6afef65b79db744bb54866419001
Arthur Hetem <arthurhetemgames@gmail.com>
2021-10-28 00:49:38 -0300

    Lesson 28

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PrincipalController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,14 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'Olá, sejam bem-vindos ao curso';
-});
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
 
-Route::get('/sobre-nos', function () {
-    return 'Sobre nós';
-});
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
 
-Route::get('/contato', function () {
-    return 'Contato';
-});
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
