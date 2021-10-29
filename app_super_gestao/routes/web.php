a613ac61917b2e3f3407c4802257847bbfc14fbd
Arthur Hetem <arthurhetemgames@gmail.com>
2021-10-29 00:40:31 -0300

    Lesson 40

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -25,11 +25,8 @@ Route::prefix('/app')->group(function() {
   Route::get('/produtos', function() { return 'Produtos';})->name('app.produtos');
 });
 
-Route::get('/rota1', function() { return 'Rota 1';})->name('site.rota1');
+Route::get('/teste/{p1}/{p2}', [\App\Http\Controllers\TesteController::class, 'teste'])->name('teste');
 
-Route::get('/rota2', function() { 
-  return redirect()->route('site.rota1');
-})->name('site.rota2');
 
 Route::fallback(function() {
   return 'A rota não existe. <a href"'.route('site.index').'">Clique aqui</a> para ir para página inicial';
