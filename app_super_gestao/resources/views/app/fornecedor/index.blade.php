aa30670f15937872e28bee5c44058128afc73cb3
Arthur Hetem <arthurhetemgames@gmail.com>
2021-10-30 00:37:10 -0300

    Lesson 46

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -2,20 +2,20 @@
 
 @php
   /*
-  if() {
-
-  } elseif() {
-
-  } else {
+  if( !<condicao>) {
 
   }
   */
 @endphp
+{{-- @unless execute se o retorno for falso --}}
 
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-  <h3>Existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores) >= 10)
-  <h3>Existem vários fornecedores cadastrados</h3>
-@else
-  <h3>Ainda não existem fornecedores cadastrados</h3>
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br />
+Status: {{ $fornecedores[0]['status'] }}
+@if( !($fornecedores[0]['status'] == 'N') )
+  Fornecedor inativo
 @endif
+<br />
+@unless($fornecedores[0]['status'] == 'S')
+  Fornecedor inativo
+@endunless
