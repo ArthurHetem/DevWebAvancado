9f836c42187ba16c05e7b5783ea8ae2e3ff6d37d
Arthur Hetem <arthurhetemgames@gmail.com>
2021-10-30 22:12:35 -0300

    Lesson 50

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -12,17 +12,14 @@ class FornecedorController extends Controller
             0 => [
                 'nome' => 'Fornecedor 1',
                 'status' => 'N',
-                'cnpj' => '00'
+                'cnpj' => '0'
             ],
             1 => [
                 'nome' => 'Fornecedor 2',
-                'status' => 'S'
+                'status' => 'S',
+                'cnpj' => 'abcd'
             ]
         ];
-
-        $msg = isset($fornecedores[0]['cnpj']) ? 'CNPJ informado' : 'CNPJ não informado';
-        echo $msg;
-
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
