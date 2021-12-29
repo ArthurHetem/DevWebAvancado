678868276b480ffdfe8c60adda8000bb689de227
Arthur Hetem <arthurhetemgames@gmail.com>
2021-12-29 09:48:26 -0300

    Lesson 134

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -3,12 +3,59 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SiteContato;
+use PhpParser\Node\Expr\FuncCall;
+use App\Models\MotivoContato;
 
 class ContatoController extends Controller
 {
-    public function contato() {
+    public function contato(Request $request) {
 
-        var_dump($_POST);
-        return view('site.contato');
+        /*$contato = new SiteContato();
+
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('nmotivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+
+        $contato->save();*/
+
+        //$contato = new SiteContato();
+
+        /*$contato->fill($request->all());
+
+        $contato->save();*/
+
+        //$contato->create($request->all());
+
+        $motivos_contato = MotivoContato::all();
+
+
+        return view('site.contato', ['title' => 'Contato', 'motivos_contato' => $motivos_contato]);
+    }
+
+    public function salvar(Request $request) {
+        $request->validate(
+                [
+                    'nome' => 'required|min:3|max:40',
+                    'telefone' => 'required',
+                    'email' => 'email',
+                    'motivos_contato_id' => 'required',
+                    'mensagem' => 'required|max:2000',
+                ],
+                [
+                    'nome.required' => 'O campo nome é obrigatório',
+                    'nome.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+                    'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+                    'telefone.required' => 'O campo telefone é obrigatório',
+                    'email.email' => 'O campo email deve ser um email válido',
+                    'motivos_contato_id.required' => 'O campo motivo de contato é obrigatório',
+                    'mensagem.required' => 'O campo mensagem é obrigatório',
+                    'mensagem.max' => 'O campo mensagem deve ter no máximo 2000 caracteres',
+                ]
+     );
+        SiteContato::create($request->all());
+        return redirect()->route('site.index')->with('success', 'Mensagem enviada com sucesso!');
     }
 }
--- app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
+++ app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,20 +1,35 @@
 {{ $slot }}
 <form action={{ route('site.contato') }} method="POST">
   @csrf
-  <input name="nome" type="text" placeholder="Nome" class="borda-preta">
+  <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="borda-preta">
+    @if($errors->has('nome'))
+    <span class="erro">{{ $errors->first('nome') }}</span>
+    @endif
   <br>
-  <input name="telefone" type="text" placeholder="Telefone" class="borda-preta">
+  <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="borda-preta">
+    @if($errors->has('telefone'))
+    <span class="erro">{{ $errors->first('telefone') }}</span>
+    @endif
   <br>
-  <input name="email" type="text" placeholder="E-mail" class="borda-preta">
+  <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="borda-preta">
+    @if($errors->has('email'))
+    <span class="erro">{{ $errors->first('email') }}</span>
+    @endif
   <br>
-  <select name="motivoContato" class="borda-preta">
-      <option value="">Qual o motivo do contato?</option>
-      <option value="1">Dúvida</option>
-      <option value="2">Elogio</option>
-      <option value="3">Reclamação</option>
+    <select name="motivos_contato_id" class="borda-preta">
+        <option value="">Qual o motivo do contato?</option>
+        @foreach($motivos_contato as $motivo)
+            <option value="{{ $motivo->id }}" {{ old('motivos_contato_id') == $motivo->id ? 'selected' : '' }}>{{ $motivo->motivo_contato }}</option>
+        @endforeach
   </select>
+    @if($errors->has('motivos_contato_id'))
+    <span class="erro">{{ $errors->first('motivos_contato_id') }}</span>
+    @endif
   <br>
-  <textarea name="mensagem" class="borda-preta">Preencha aqui a sua mensagem</textarea>
+  <textarea name="mensagem" class="borda-preta">{{ (old('mensagem') != '') ? old('mensagem') : 'Preencha aqui a sua mensagem' }}</textarea>
+    @if($errors->has('mensagem'))
+        <span class="erro">{{ $errors->first('mensagem') }}</span>
+    @endif
   <br>
   <button type="submit" class="borda-preta">ENVIAR</button>
 </form>
