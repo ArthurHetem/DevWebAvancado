b322f0185baa94d9ff9f6b11ebeb9dab47ac3cb8
Arthur Hetem <arthurhetemgames@gmail.com>
2022-04-12 13:15:48 -0300

    Lesson 376

--- app_locadora_carros/resources/js/app.js
+++ app_locadora_carros/resources/js/app.js
@@ -8,6 +8,17 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+
+    }
+});
+
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -38,4 +49,5 @@ Vue.component('paginate-component', require('./components/Paginate.vue').default
 
 const app = new Vue({
     el: '#app',
+    store
 });
